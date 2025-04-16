24865329d8b4a6f0a384c003ef6a98f868d7d5c5
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-04-16 22:40:13 +0200

    renamed changed fields

--- repository/hotel_repository.py
+++ repository/hotel_repository.py
@@ -15,9 +15,6 @@ class HotelRepository(BaseRepository):
     def __init__(self):
         super().__init__(SessionLocal(), Hotel)
 
-    def get_by_address_id(self, address_id):
-        return self.db.query(self.model).filter(self.model.address_id == address_id).first()
-
     def get_filtered(self, city: Optional[str], min_stars: Optional[int], capacity=None, check_in=None, check_out=None):
         query = self.db.query(Hotel).join(Hotel.rooms).join(Room.type).join(Hotel.address).options(
             joinedload(Hotel.rooms).joinedload(Room.type),
@@ -35,9 +32,9 @@ class HotelRepository(BaseRepository):
 
         if check_in and check_out:
             subquery = select(Booking.room_id).where(
-                Booking.check_in_date < check_out,
-                Booking.check_out_date > check_in
+                Booking.check_in < check_out,
+                Booking.check_out > check_in
             )
-            query = query.filter(~Room.room_id.in_(subquery))
+            query = query.filter(~Room.id.in_(subquery))
 
         return query.distinct().all()
