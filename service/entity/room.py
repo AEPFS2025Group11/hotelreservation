c5bacfa8713717d326558ac74735d56a2b0acbfc
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-04-16 16:40:15 +0200

    removed unused capacity field of room entity

--- service/entity/room.py
+++ service/entity/room.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, String, Double, Float
 from sqlalchemy.orm import relationship
 
 from app.util.base import Base
@@ -9,10 +9,9 @@ class Room(Base):
 
     room_id = Column(Integer, primary_key=True, autoincrement=True)
     hotel_id = Column(Integer, ForeignKey('hotel.hotel_id'), nullable=False)
-    room_number = Column(Integer, nullable=False, index=True)
+    room_number = Column(String, nullable=False, index=True)
     type_id = Column(Integer, ForeignKey('room_type.type_id'), nullable=False)
-    price_per_night = Column(Integer, nullable=False)
-    capacity = Column(Integer, nullable=False)
+    price_per_night = Column(Float, nullable=False)
 
     hotel = relationship("Hotel", back_populates="rooms", lazy="joined")
 
