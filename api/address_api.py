742f6ce54499ddd49a938aea10214328916a1b8f
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-04-16 16:39:38 +0200

    changed url of addresses

--- api/address_api.py
+++ api/address_api.py
@@ -3,15 +3,15 @@ from fastapi import APIRouter
 from app.service.address_service import AddressService
 from app.service.models.address_models import AddressOut, AddressIn
 
-router = APIRouter(prefix="/api", tags=["addresses"])
+router = APIRouter(prefix="/api/addresses", tags=["addresses"])
 address_service = AddressService()
 
 
-@router.get("/addresses", response_model=list[AddressOut])
+@router.get("/", response_model=list[AddressOut])
 async def get_addresses():
     return address_service.get_all()
 
 
-@router.post("/addresses", response_model=AddressOut)
+@router.post("/", response_model=AddressOut)
 async def create_address(address: AddressIn):
     return address_service.create(address.model_dump())
