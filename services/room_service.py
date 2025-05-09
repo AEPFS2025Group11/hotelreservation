b99b0b473a78832944c632b24093778f9d652f74
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-05-09 16:06:49 +0200

    bugfix

--- services/room_service.py
+++ services/room_service.py
@@ -84,9 +84,15 @@ class RoomService:
         if room_type is None:
             logger.warning(f"Room type with ID {room.type_id} not found")
             raise HTTPException(status_code=404, detail="Room type not found")
-        room_entity = Room(**room.model_dump())
         facilities = self.facility_repo.get_by_ids(room.facility_ids)
-        room_entity.facility_ids = facilities
+
+        room_entity = Room(
+            room_number=room.room_number,
+            price_per_night=room.price_per_night,
+            type_id=room.type_id,
+            hotel_id=room.hotel_id,
+        )
+        room_entity.facilities = facilities
         print(room_entity)
         created_room = self.room_repo.create(room_entity)
         if created_room is None:
