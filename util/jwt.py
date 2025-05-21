ac2b6fd2f14a1a4bfae3d0bc846a259a079d0201
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-05-21 15:04:26 +0200

    Removed unused code

--- util/jwt.py
+++ util/jwt.py
@@ -1,11 +1,6 @@
 from datetime import datetime, timedelta
 
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
-
-from app.database.database import SessionLocal
-from app.entities.user import User
 
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
@@ -25,27 +20,3 @@ def decode_token(token: str):
         return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     except JWTError:
         return None
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
-
-
-def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
-    payload = decode_token(token)
-    if not payload:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid or expired token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    user_id = payload.get("sub")
-    if not user_id:
-        raise HTTPException(status_code=401, detail="Token fehlt für User ID.")
-
-    db = SessionLocal()
-    user = db.query(User).filter(User.id == int(user_id)).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User konnte nicht gefunden werden.")
-
-    return user
