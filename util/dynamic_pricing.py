f2a78236f06ad0ed42e98901a37236621a6c6b75
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-06-15 13:41:35 +0200

    added more months to the dynamic pricing

--- util/dynamic_pricing.py
+++ util/dynamic_pricing.py
@@ -9,9 +9,9 @@ logging.basicConfig(level=logging.INFO)
 def calculate_dynamic_price(base_price: float, check_in: Optional[date]) -> float:
     if not check_in:
         return base_price
-    if check_in.month in [7, 8]:
+    if check_in.month in [7, 8, 9]:
         return round(base_price * 1.2, 2)
-    elif check_in.month in [1, 2, 11]:
+    elif check_in.month in [1, 2, 11, 12]:
         return round(base_price * 0.9, 2)
     else:
         return base_price
